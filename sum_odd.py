2dd0682dd91c4ba360b96de25371890ee9742eab
elmurod <elmurodraimov764@gmail.com>
2023-10-22 20:28:13 +0500

    code

--- sum_odd.py
+++ sum_odd.py
@@ -1,6 +1,10 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+number = 5678
+n1 = 8
+n2 = 7
+n3 = 6
+n4 = 5
 #Create a variable "sum_odd" and assign it 0.
-
+print(n2 + n4)
 #Find the sum of the odd digits in the variable "var_int".
