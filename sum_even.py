298b530fee38b505b08873f1312b8bc0149fa44d
elmurod <elmurodraimov764@gmail.com>
2023-10-23 18:54:30 +0500

    code

--- sum_even.py
+++ sum_even.py
@@ -1,10 +1,18 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-number = 5678
-n1 = 8
-n2 = 7
-n3 = 6
-n4 = 5
+n = 5637
+
+n1 = n%10
+n = n//10
+
+n2 =n%10
+n = n//10
+
+n3 = n%10
+n = n//10
+
+n4 = n%10
+x = ( n1%2*n1 +  n2%2 * n2 +  n3%2* n3 +  n4%2 * n4)
 #Create a variable "sum_even" and assign it 0.
-print(n1 + n3)
+print(x)
 #Find the sum of the even digits in the variable "var_int".
